fb2ba10fb90f574ba05f2c163d0a9d5df81aab51
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-22 14:50:24 +0800

    Initial Commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,10 +6,9 @@ export default function Home() {
   return (
     <Layout>
       <div style={{ color: `purple` }}>
-        <h1>This is the home page</h1>
-        <Link to="/contact/">Contact</Link>
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <h1>This is the contact page</h1>
+        <Link to="/">Home</Link>
+        <p>What a send me a message, yo!</p>
       </div>
     </Layout>
   )
